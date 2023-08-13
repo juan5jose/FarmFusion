75a6a0ca4fafe4981e7ff2fab8f9a6d7eccd995a
andresfelipecespedesgomez@gmail.com <andresfelipecespedesgomez@gmail.com>
2023-08-13 17:32:47 -0500

    registroo

---
+++ java.js
@@ -0,0 +1,14 @@
+
+
+    let usuario, email, contraseña;
+    usuario = document.getElementById("user");
+    email = document.getElementById("email");
+    contraseña = document.getElementById("contraseña");
+    
+    
+    if (usuario === '') {
+        alert("el campo usuario es requerido");
+        
+    }
+
+a
