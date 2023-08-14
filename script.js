34b419b97692e459ac94b7ed67a91de14f7e8d93
andresfelipecespedesgomez@gmail.com <andresfelipecespedesgomez@gmail.com>
2023-08-13 19:41:57 -0500

    registrar fn

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-const user = document.getElementById("usuario");
-const email= document.getElementById("email");
-const pass= document.getElementById("contraseña");
+function validar (){
+
+    let user = document.getElementById("user");
+    let email= document.getElementById("email");
+    let pass= document.getElementById("contraseña");
+    let form= document.getElementById("form")
+    
+    if (user.length <6){
+        alert ("los campos usuario, email y contraseña son requeridos");
+    }
+}
+
+
