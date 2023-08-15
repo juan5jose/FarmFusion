595ec6681663c564d5df1017dcbf62c3ecdc1535
juan5jose <juanjosecas2109@gmail.com>
2023-08-15 17:37:45 -0500

    Organizo

--- js/script.js
+++ js/script.js
@@ -15,3 +15,18 @@ const button = document.querySelector('.button');
 button.addEventListener('click', () => {
   window.location.href = 'login.html';
 });
+
+//js registro.html
+function validar (){
+
+    let user = document.getElementById("user");
+    let email= document.getElementById("email");
+    let pass= document.getElementById("pass");
+    let form= document.getElementById("form");
+    let boton = document.getElementById("boton");
+    
+    if (user === '' ){
+        alert("el campo usuario es requerido");
+    }
+
+}
